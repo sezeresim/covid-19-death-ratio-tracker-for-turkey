bdfc6727e78d63b02006d86f13e381af849b7bd9
sezeresim <sezeresim@gmail.com>
2020-04-24 01:33:48 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from "react";
+import './App.css'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages:[],
+    };
+  }
+
+componentDidMount = async () => { 
+  const response =await axios.get('https://pomber.github.io/covid19/timeseries.json');
+  console.log(response.data["Turkey"]);
+  this.setState({ 
+    pages: response.data["Turkey"], 
+  }); 
+};
+
+
+render() {
+   const posts = this.state.pages.reverse().map(post => (
+     <div className="alert alert-primary col-md-12">
+        <strong className="text text-danger">{post.deaths/post.confirmed}</strong> 
+        <hr/>
+        <p>{post.date}</p>
+     </div>
+   ));
+
+ return(
+  <div className="container">
+   <h2>Covid19 Death Ratio</h2>
+   <hr/>
+    <div className="row">
+      {posts}
     </div>
-  );
+  </div>
+ );
+}
 }
 
 export default App;
