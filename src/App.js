7fd7f89a3e069c25f3ee3d9390c10103d6621a69
sezeresim <sezeresim@gmail.com>
2020-04-24 02:37:44 +0300

    supported graph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React,{ Component } from "react";
 import './App.css'
 import axios from 'axios';
+import CanvasJSReact  from './canvasjs.react';
+let CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class App extends Component{
   constructor(props) {
@@ -12,7 +14,6 @@ class App extends Component{
 
 componentDidMount = async () => { 
   const response =await axios.get('https://pomber.github.io/covid19/timeseries.json');
-  console.log(response.data["Turkey"]);
   this.setState({ 
     pages: response.data["Turkey"], 
   }); 
@@ -27,13 +28,50 @@ render() {
         <p>{post.date}</p>
      </div>
    ));
+   
+    const dataPoint=[];
+      this.state.pages.reverse().map(post => {
+        let k=post.deaths/post.confirmed;
+        let p=post.date;
+        dataPoint.push({
+          y: k,
+          label: p
+        })
+    });
+    
+    console.log(dataPoint)
 
+   const options = {
+    animationEnabled: true,	
+    title:{
+      text: "Graph for Death Ratio"
+    },
+    axisY : {
+      title: "Ratio Rate",
+      includeZero: true
+    },
+    toolTip: {
+      shared: true
+    },
+    data: [{
+      type: "spline",
+      name: "2020",
+      showInLegend: true,
+      dataPoints: dataPoint.slice(54)
+    }]
+}
  return(
   <div className="container">
    <h2>Covid19 Death Ratio</h2>
+   <div>
+			<CanvasJSChart options = {options} 
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
    <hr/>
     <div className="row">
-      {posts}
+      {/* {posts} */}
     </div>
   </div>
  );
